7b3917fb4734d28dbcb92c9d9c692ab25dc7f42c
SamikshaD <samikshadhomne@gmail.com>
2016-06-20 01:14:40 +0530

    Commiting VehicleIdentifierClient

--- vehicle-identifier/src/main/java/com/workscape/vehicleidentifier/VehicleIdentifierClient.java
+++ vehicle-identifier/src/main/java/com/workscape/vehicleidentifier/VehicleIdentifierClient.java
@@ -1,14 +1,12 @@
 package com.workscape.vehicleidentifier;
 
-import javax.xml.bind.JAXBException;
-
 /**
  * Client Class for Vehicle Identifier
  * 
  */
 public class VehicleIdentifierClient {
 
-	public static void main(String[] args) throws JAXBException{
+	public static void main(String[] args) {
 		VehicleIdentifier vehicleIdentifier = new VehicleIdentifier();
 		vehicleIdentifier.vehicleIdentifier();
 	}
